8088dfac373eb7c54fab9bcdf000128fc439bc53
Sam Hession <samhession@live.co.uk>
2020-06-24 10:25:16 +0100

    Adds additional values to Thrift conversion models

--- src/main/scala/com/gu/octopusthrift/models/OctopusArticle.scala
+++ src/main/scala/com/gu/octopusthrift/models/OctopusArticle.scala
@@ -3,23 +3,28 @@ package com.gu.octopusthrift.models
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import com.gu.flexibleoctopus.model.thrift._
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 /**
-  * This model does not represent all fields sent by Octopus,
-  * only those that will be relevant in converting to our Thrift model
-  */
+ * This model does not represent all fields sent by Octopus,
+ * only those that will be relevant in converting to our Thrift model
+ */
 case class OctopusArticle(
-    id: Int,
-    filename: String,
-    forPublication: String,
-    lawyered: String,
-    objectType: String,
-    objectNumber: Int,
-    lastModified: String,
-    inUseBy: Option[String],
-    isCheckedOut: String,
-    status: String
-) {
+  id: Int,
+  filename: String,
+  forPublication: String,
+  lawyered: String,
+  objectType: String,
+  objectNumber: Int,
+  lastModified: String,
+  inUseBy: Option[String],
+  isCheckedOut: String,
+  status: String,
+  attachedTo: Option[String],
+  onPages: Option[String]) {
+  def lastModifiedEpoch = DateTime.parse(lastModified, DateTimeFormat.forPattern("yyyyMMddHHmm")).getMillis
+  def pageNumber = onPages.map(_.split(',')(0).split(';')(0).toLong)
   def as[T](implicit f: OctopusArticle => T) = f(this)
 }
 
@@ -34,7 +39,9 @@ object OctopusArticle {
     (__ \ "last_modified").read[String] and
     (__ \ "in_use_by").readNullable[String] and
     (__ \ "ischeckedout").read[String] and
-    (__ \ "status").read[String])(OctopusArticle.apply _)
+    (__ \ "status").read[String] and
+    (__ \ "attached_to").readNullable[String] and
+    (__ \ "on_pages").readNullable[String])(OctopusArticle.apply _)
 
   implicit def articleMapper = (octopusArticle: OctopusArticle) => {
 
@@ -44,8 +51,7 @@ object OctopusArticle {
       "n" -> Lawyered.Notapplicable,
       "p" -> Lawyered.Pending,
       "c" -> Lawyered.Cleared,
-      "r" -> Lawyered.Priority
-    )
+      "r" -> Lawyered.Priority)
 
     val articleStatus = Map(
       "Chief Sub" -> ArticleStatus.Chiefsub,
@@ -55,8 +61,7 @@ object OctopusArticle {
       "Killed" -> ArticleStatus.Killed,
       "Revise Sub" -> ArticleStatus.Revisesub,
       "Subs" -> ArticleStatus.Subs,
-      "Writers" -> ArticleStatus.Writers
-    )
+      "Writers" -> ArticleStatus.Writers)
 
     val isCheckedOut = Map("Y" -> true, "N" -> false)
 
@@ -65,10 +70,9 @@ object OctopusArticle {
       forPub(octopusArticle.forPublication),
       octopusArticle.inUseBy,
       isCheckedOut(octopusArticle.isCheckedOut),
-      octopusArticle.lastModified.toLong,
+      octopusArticle.lastModifiedEpoch,
       lawyered(octopusArticle.lawyered),
       articleStatus(octopusArticle.status),
-      octopusArticle.filename
-    )
+      octopusArticle.filename)
   }
 }
--- src/main/scala/com/gu/octopusthrift/models/OctopusBundle.scala
+++ src/main/scala/com/gu/octopusthrift/models/OctopusBundle.scala
@@ -5,21 +5,23 @@ import play.api.libs.functional.syntax._
 import com.gu.octopusthrift.models.OctopusArticle
 import com.gu.flexibleoctopus.model.thrift._
 import com.gu.octopusthrift.services.ArticleFinder
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 /**
-  * This model does not represent all fields sent by Octopus,
-  * only those that will be relevant in converting to our Thrift model
-  */
+ * This model does not represent all fields sent by Octopus,
+ * only those that will be relevant in converting to our Thrift model
+ */
 case class OctopusBundle(
-    id: Int,
-    info8: String,
-    pubCode: String,
-    pubDate: String,
-    sectionCode: String,
-    articles: Array[OctopusArticle]
-) {
+  id: Int,
+  info8: String,
+  pubCode: String,
+  pubDate: Option[String],
+  sectionCode: String,
+  articles: Array[OctopusArticle]) {
   private val composerIdLocation = 7
   def composerId: String = info8.split(',')(composerIdLocation).trim
+  def pubDateEpochDays = pubDate.map(date => DateTime.parse(date, DateTimeFormat.forPattern("yyyyMMdd")).getMillis)
   def as[T](implicit f: OctopusBundle => T) = f(this)
 }
 
@@ -27,7 +29,7 @@ object OctopusBundle {
   implicit val reads = ((__ \ "id").read[Int] and
     (__ \ "info8").read[String] and
     (__ \ "pubcode").read[String] and
-    (__ \ "pubdate").read[String] and
+    (__ \ "pubdate").readNullable[String] and
     (__ \ "sectioncode").read[String] and
     (__ \ "articles").read[Array[OctopusArticle]])(OctopusBundle.apply _)
 
@@ -35,18 +37,17 @@ object OctopusBundle {
 
     val bodyText = ArticleFinder.findBodyText(octopusBundle)
     val article = bodyText.get.as[Article]
+    val headline = None
 
-    // TODO: fully populate model with bundle values
     StoryBundle(
       octopusBundle.id,
       octopusBundle.composerId,
       octopusBundle.pubCode,
       octopusBundle.sectionCode,
       article,
-      None,
-      None,
-      None,
-      None
-    )
+      headline,
+      bodyText.flatMap(_.pageNumber),
+      octopusBundle.pubDateEpochDays,
+      bodyText.flatMap(_.attachedTo))
   }
 }
