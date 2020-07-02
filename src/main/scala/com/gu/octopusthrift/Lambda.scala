712237b26eaecd832eb57332a6677db0389acaaf
jennygrahamjones <jennygrahamjones@users.noreply.github.com>
2020-07-02 09:55:31 +0100

    Changed shape and handling of Octopus payload

--- src/main/scala/com/gu/octopusthrift/Lambda.scala
+++ src/main/scala/com/gu/octopusthrift/Lambda.scala
@@ -7,7 +7,11 @@ import com.gu.flexibleoctopus.model.thrift._
 import com.gu.octopusthrift.aws.{ CustomMetrics, DeadLetterQueue, Kinesis, Metrics }
 import com.gu.octopusthrift.models._
 import com.gu.octopusthrift.services.Logging
-import com.gu.octopusthrift.services.PayloadValidator.{ isValidBundle, validatePayload }
+import com.gu.octopusthrift.services.PayloadValidator.{
+  isValidBundle,
+  validateCachePayload,
+  validateSinglePayload
+}
 import com.gu.octopusthrift.util.ThriftSerializer.serializeToBytes
 import play.api.libs.json._
 
@@ -22,29 +26,26 @@ object Lambda extends Logging with CustomMetrics with DeadLetterQueue {
     records.foreach(record => {
       val sequenceNumber = record.getSequenceNumber
       logger.info(s"Received payload, sequence number: $sequenceNumber")
-
       val data = record.getData().array()
-      val validatedPayload: Option[OctopusPayload] = validatePayload(data)
 
-      validatedPayload.foreach(payload => {
-        logger.info(s"Validated payload, sequence number: $sequenceNumber")
-        if (payload.bundles.isDefined) {
-          val messageIndex = payload.thismessageindex.getOrElse(0)
-          val totalMessages = payload.totalmessages.getOrElse(0)
-          logger.info(
-            s"Processing daily snapshot, message $messageIndex of $totalMessages, sequence number: $sequenceNumber"
-          )
-          payload.bundles.get.foreach(bundle => processBundle(bundle, sequenceNumber))
-        } else if (payload.data.isDefined) {
+      (validateSinglePayload(data), validateCachePayload(data)) match {
+        case (Some(singleBundle), _) =>
           logger.info(s"Processing single story bundle, sequence number: $sequenceNumber")
-          processBundle(payload.data.get, sequenceNumber)
-        } else {
-          logger.info(s"Payload does not contain expected data, sequence number: $sequenceNumber")
-          cloudWatch.publishMetricEvent(Metrics.MissingExpectedPayloadData)
-          deadLetterQueue.sendMessage(Json.toJson(payload))
+          processBundle(singleBundle.data.get, sequenceNumber)
+        case (None, Some(cache)) => {
+          val cacheData = cache.data.get
+          val messageIndex = cacheData.thismessageindex.getOrElse(0)
+          val totalMessages = cacheData.totalmessages.getOrElse(0)
+          logger.info(
+            s"Processing daily snapshot, message $messageIndex of $totalMessages, sequence number: $sequenceNumber")
+          cacheData.bundles.get.foreach(bundle => processBundle(bundle, sequenceNumber))
         }
-        logger.info(s"Completed handling validated payload, sequence number: $sequenceNumber")
-      })
+        case _ => {
+          logger.info(s"Payload does not match OctopusPayload model, sequence number: $sequenceNumber")
+          deadLetterQueue.sendMessage(Json.toJson(data))
+          cloudWatch.publishMetricEvent(Metrics.InvalidOctopusPayload)
+        }
+      }
     })
   }
 
@@ -59,8 +60,7 @@ object Lambda extends Logging with CustomMetrics with DeadLetterQueue {
           stream.publish(serializedThriftBundle)
         case Failure(e) =>
           logger.info(
-            s"Bundle failed validation as StoryBundle, sequence number: $sequenceNumber, with error: $e"
-          )
+            s"Bundle failed validation as StoryBundle, sequence number: $sequenceNumber, with error: $e")
           cloudWatch.publishMetricEvent(Metrics.FailedThriftConversion)
           deadLetterQueue.sendMessage(Json.toJson(octopusBundle))
       }
